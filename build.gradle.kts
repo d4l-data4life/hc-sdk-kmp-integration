20211369b3f4ba2ed343729843d9792544a52b81
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2020-05-19 10:58:34 +0200

    Update Gradle 6.1.1 -> 6.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,6 @@ tasks.register("clean", Delete::class.java) {
 }
 
 tasks.named<Wrapper>("wrapper") {
-    gradleVersion = "6.1.1"
+    gradleVersion = "6.4.1"
     distributionType = Wrapper.DistributionType.ALL
 }
