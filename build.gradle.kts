92606c6c30f0ec49e5a1a144d038deacf0b3d3f0
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2020-09-03 22:35:04 +0200

    Bump HC-SDK 1.6.0 -> 1.6.1, FHIR SDK 0.6.0 -> 0.6.1, Kotlin 1.3.72 -> 1.4.0, Kotlin Coroutines 1.3.6 -> 1.3.9, Dependency Update Gradle Plugin 0.28.0 -> 0.29.0, AndroidX KTX 1.2.0 -> 1.3.1, AndroidX AppCompat 1.1.0 -> 1.2.0, AndroidX ContraintLayout 2.0.0-beta4 -> 2.0.1, AndroidX Navigation 2.2.0 -> 2.3.0, Google Material 1.1.0 -> 1.2.1, OkHttp 4.3.1 -> 4.8.1, Retrofit 2.7.1 -> 2.9.0, Gson 2.7.1 -> 2.9.0, Chucker 3.1.1 -> 3.2.0, AndroidX Test Core 1.0.0 -> 1.3.0, AndroidX Test 1.1.0 -> 1.3.0, AndroidX Espresso 3.1.1 -> 3.3.0, AndroidX Text Extension 1.1.0 -> 1.1.2, Kakao 2.3.2 -> 2.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,15 @@ allprojects {
         maven("https://jitpack.io")
         jcenter()
     }
+
+    // FIXME remove if dependency conflict is solved
+    configurations.all {
+        resolutionStrategy {
+            force(Dependencies.Android.okHttp)
+            force(Dependencies.Android.okHttpLoggingInterceptor)
+            force(Dependencies.Android.retrofit)
+        }
+    }
 }
 
 tasks.register("clean", Delete::class.java) {
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -3,16 +3,16 @@
  */
 
 object Versions {
-    const val hcSdk = "1.6.0"
+    const val hcSdk = "1.6.1"
 
     /**
      * [D4L-FHIR-SDK]https://github.com/gesundheitscloud/hc-fhir-android)
      */
-    const val fhirSdk = "0.6.0"
+    const val fhirSdk = "0.6.1"
     const val fhirHelper = "1.2.4"
 
-    const val kotlin = "1.3.72"
-    const val kotlinCoroutines = "1.3.6"
+    const val kotlin = "1.4.0"
+    const val kotlinCoroutines = "1.3.9"
 
     object GradlePlugins {
         const val kotlin = Versions.kotlin
@@ -26,51 +26,48 @@ object Versions {
         /**
          * [DependencyUpdates](https://github.com/ben-manes/gradle-versions-plugin)
          */
-        const val dependencyUpdates = "0.28.0"
+        const val dependencyUpdates = "0.29.0"
     }
 
     // Android
     const val androidDesugar = "1.0.5"
 
     // AndroidX
-    const val androidXKtx = "1.2.0"
-    const val androidXAppCompat = "1.1.0"
+    const val androidXKtx = "1.3.1"
+    const val androidXAppCompat = "1.2.0"
     const val androidXBrowser = "1.2.0"
 
-    const val androidXConstraintLayout = "2.0.0-beta4"
+    const val androidXConstraintLayout = "2.0.1"
 
     const val androidXLifecycle = "2.1.0"
-    const val androidXNavigation = "2.2.0"
+    const val androidXNavigation = "2.3.0"
 
     // Material
-    const val material = "1.1.0"
-
-    // Google
-    const val googlePlayServices = "15.0.1"
+    const val material = "1.2.1"
 
     // Network
     /**
      * [okHttp](https://github.com/square/okhttp)
      */
-    const val okHttp = "4.3.1"
+    const val okHttp = "4.8.1"
 
     /**
      *
      *[retrofit](https://github.com/square/retrofit)
      */
-    const val retrofit = "2.7.1"
+    const val retrofit = "2.9.0"
 
     /**
      *
      *[gson](https://github.com/square/retrofit/tree/master/retrofit-converters/gson)
      */
-    const val gson = "2.7.1"
+    const val gson = "2.9.0"
 
     /**
      *
      *[chucker inspector](//https://github.com/ChuckerTeam/chucker/tree/develop)
      */
-    const val chucker = "3.1.1"
+    const val chucker = "3.2.0"
 
     /**
      * [ThreeTen Android Backport](https://github.com/JakeWharton/ThreeTenABP)
@@ -83,14 +80,14 @@ object Versions {
 
 
     // Android Test
-    const val androidXTestCore = "1.0.0"
-    const val androidXTest = "1.1.0"
-    const val androidXEspresso = "3.1.1"
+    const val androidXTestCore = "1.3.0"
+    const val androidXTest = "1.3.0"
+    const val androidXEspresso = "3.3.0"
     const val androidXUiAutomator = "2.2.0"
-    const val androidXTestExtJUnit = "1.1.0"
+    const val androidXTestExtJUnit = "1.1.2"
 
     // https://github.com/agoda-com/Kakao
-    const val androidTestKakao = "2.3.2"
+    const val androidTestKakao = "2.3.4"
 
     // https://github.com/KasperskyLab/Kaspresso
     const val androidTestKaspresso = "1.1.0"
