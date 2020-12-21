46210090df1e092fbc54ec4cf85d6e252f830136
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2020-12-21 15:26:55 +0100

    Bump Gradle 6.7 -> 6.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,6 @@ tasks.register("clean", Delete::class.java) {
 }
 
 tasks.named<Wrapper>("wrapper") {
-    gradleVersion = "6.7"
+    gradleVersion = "6.7.1"
     distributionType = Wrapper.DistributionType.ALL
 }
