1d1510868b71d479dd65fc523041a137f27e06a4
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2021-05-04 09:14:44 +0200

    Change emulator screen size to be able to render full auth webpage

--- ci/android/prepare_start_and_wait_for_emulator.sh
+++ ci/android/prepare_start_and_wait_for_emulator.sh
@@ -29,7 +29,7 @@ run_headless_emulator() {
     -no-accel \
     -no-audio \
     -no-boot-anim \
-    -skin 768x1980 \
+    -skin 768x2560 \
     -camera-back none \
     -camera-front none &
 }
@@ -40,7 +40,7 @@ run_emulator() {
     -no-snapshot \
     -no-audio \
     -no-boot-anim \
-    -skin 768x1980 \
+    -skin 768x2560 \
     -camera-back none \
     -camera-front none &
 }
