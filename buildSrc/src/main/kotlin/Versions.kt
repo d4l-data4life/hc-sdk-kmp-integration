8230873e26c7dc66b7c55cdb34087edd03984f5a
Wolf-Martell Montwe <wolf@wolfmontwe.de>
2022-04-14 16:49:59 +0200

    Bump Android Gradle Plugin 7.1.3 -> 7.2.0-beta04 for Android Integration tests working with compose

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -31,7 +31,7 @@ object Versions {
 
     object GradlePlugins {
         const val kotlin = Versions.kotlin
-        const val android = "7.1.3"
+        const val android = "7.2.0-beta04"
 
         const val jUnit5Android = "1.8.2.0"
 
